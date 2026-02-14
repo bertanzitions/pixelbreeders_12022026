2fe48737f18b71bd7337abe07f96b34aa1288c23
bertanzitions <bertan1707@gmail.com>
2026-02-15 00:14:04 +0100

    ✨Auth in backend

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,28 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from models import db
+
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 CORS(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["JWT_SECRET_KEY"] = "pixelbreeders"
+
+jwt = JWTManager(app)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+app.register_blueprint(auth_bp, url_prefix='/auth') 
+
 @app.route('/', methods=['GET'])
-def hello_world():
-    return jsonify({
-        "message": "Hello World!"
-    })
+def hello():
+    return jsonify({"msg": "Hello world!"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ backend/models.py
@@ -0,0 +1,56 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    
+    ratings = db.relationship('Rating', backref='user', lazy=True)
+
+    def set_password(self, password):
+        """Creates a hash for the password"""
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        """Checks if the provided password matches the hash"""
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.email}>'
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    tmdb_id = db.Column(db.Integer, unique=True, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    poster_path = db.Column(db.String(255))
+    backdrop_path = db.Column(db.String(255))
+    release_date = db.Column(db.DateTime)
+    overview = db.Column(db.Text)
+    
+    ratings = db.relationship('Rating', backref='movie', lazy=True)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # FK's
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Rating {self.score}>'
